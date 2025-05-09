44970e3804260ea5fb4cacb030f318756503dce6
felipe798 <felipeprincipe798@gmail.com>
2025-05-08 21:02:05 -0500

    01 front

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/";
+const API_URL = "https://farmacia-back-2.onrender.com/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "test/all");
